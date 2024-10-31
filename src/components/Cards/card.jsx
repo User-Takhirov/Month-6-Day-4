a5b63e6e6bfc968769a184278384341a40df19cd
User-Takhirov <tohirovfayruz@gmail.com>
2024-10-31 17:05:07 +0500

    updated

--- src/components/Cards/card.jsx
+++ src/components/Cards/card.jsx
@@ -20,18 +20,20 @@ export const Card = () => {
             className=" bg-blue-400 rounded-[10px] w-[400px] p-[20px] mx-auto mb-[20px]"
             key={item.id}
           >
-            <h1 className="text-white mb-[10px]">{item.userList}</h1>
+            <h1 className="text-white mb-[10px] capitalize font-[600]">{item.userList}</h1>
             <div className="flex  justify-center gap-[10px]">
               <button
                 onClick={() => DeleteUser(item.id)}
-                className="bg-red-500 p-[10px] rounded-[5px] text-white"
+                className="bg-red-950 text-red-400 border border-red-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group"
               >
+                <span class="bg-red-400 shadow-red-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
                 Delete
               </button>
               <button
                 onClick={() => EditUser(item.id)}
-                className="bg-green-500 p-[10px] rounded-[5px] text-white"
+                className="bg-yellow-950 text-yellow-400 border border-yellow-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group"
               >
+                <span class="bg-yellow-400 shadow-yellow-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
                 Edit
               </button>
             </div>
